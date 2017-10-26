c0b36aaf12ca5b188305a5bfb91c1e54e24b3763
Joe Reynolds <joereynolds952@gmail.com>
2017-10-26 14:15:14 +0100

    Move array traversal out of gather_candidates

--- rplugin/python3/deoplete/sources/minisnip.py
+++ rplugin/python3/deoplete/sources/minisnip.py
@@ -11,14 +11,10 @@ class Source(Base):
         self.name = 'minisnip'
         self.mark = '[minisnip]'
         self.min_pattern_length = 0
+        self.snippets = os.listdir(str(Path.home()) + '/.vim/minisnip')
 
     def gather_candidates(self, context):
         """Returns all snippets in the users
         vim minisnip directory"""
-
-        home = str(Path.home())
-        snippets = os.listdir(home + '/.vim/minisnip')
-
-        cleaned = [snippet.split('_')[2] for snippet in snippets if context['filetype'] in snippet]
-
+        cleaned = [snippet.split('_')[2] for snippet in self.snippets if context['filetype'] in snippet]
         return [{'word': snippet} for snippet in cleaned]
