09e008d4f0e3c640c9c66ddb37271ad2d0716e5f
Yuki Iwanaga <yuki@creasty.com>
2019-03-17 15:25:58 +0900

    Fix multiple bugs
    - Defer the evaluation of `g:minisnip_dir`
    - Improve filtering logic for compound filetypes
    - Support global snippets

--- rplugin/python3/deoplete/sources/minisnip.py
+++ rplugin/python3/deoplete/sources/minisnip.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 from .base import Base
 
 class Source(Base):
@@ -9,14 +11,25 @@ class Source(Base):
         self.name = 'minisnip'
         self.mark = '[minisnip]'
         self.min_pattern_length = 0
-        self.minisnip_dir = self.vim.eval('g:minisnip_dir')
-        self.snippets = os.listdir(os.path.expanduser(self.minisnip_dir))
+        self.minisnip_dir = None
+        self.snippets = []
 
     def gather_candidates(self, context):
-        """Returns all snippets in the users
-        vim minisnip directory"""
-        filetype = '_' + context['filetype'] + '_'
+        if not self.minisnip_dir:
+            self.minisnip_dir = self.vim.eval('g:minisnip_dir')
+            self.snippets = os.listdir(os.path.expanduser(self.minisnip_dir))
 
-        cleaned = [snippet.split(filetype)[1] for snippet in self.snippets if filetype in snippet]
-        return [{'word': snippet} for snippet in cleaned]
+        ft = context['filetype']
+        ft_reg = '^_(' + format(ft.replace('.', '|')) + ')_'
 
+        candidates = []
+
+        for snippet in self.snippets:
+            if snippet.startswith('_'):
+                m = re.search(ft_reg, snippet)
+                if m:
+                    candidates.append(snippet[len(m.group(0)):])
+            else:
+                candidates.append(snippet)
+
+        return [{'word': c} for c in candidates]
